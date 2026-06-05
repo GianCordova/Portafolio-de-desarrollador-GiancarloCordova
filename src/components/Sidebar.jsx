79fe73a892f5b21e5a4997581cbec0a61e29a2a9
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-06-05 16:22:15 -0600

    Se creo un menu (Sidebar) inicial

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h2>Giancarlo C.</h2>
+      <nav>
+        <ul>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/about">Sobre mí</Link></li>
+          <li><Link to="/proyectos">Proyectos</Link></li>
+          <li><Link to="/contacto">Contacto</Link></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
