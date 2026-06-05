85c18352c7e58718c9923da802493de55c6c2107
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-06-05 16:10:01 -0600

    Se paso el proyecto del portafolio a React con Vite

---
+++ src/pages/SobreMi.jsx
@@ -0,0 +1,26 @@
+const SobreMi = () => {
+  return (
+    <main className="container">
+      <section id="sobre-mi" className="intro-box-left">
+        <h1>Sobre mí</h1>
+
+        <h2 className="section-subtitle">Un poco sobre mí...</h2>
+        <p>Soy Giancarlo Sebastian Cordova Garcia, estudiante de Perito en Informática en Fundación Kinal. Desde que entré en 2023, he aprendido muchas cosas sobre el mundo de la programación, cómo funciona, lenguajes, aplicaciones, tecnología, etc. Pero también me estoy terminando de formar como una persona profesional tanto en lo laboral como en lo ético.</p>
+        
+        <p>Decidí estudiar algo técnico por las oportunidades que brinda; me incliné por Informática ya que paso buena parte del tiempo en la computadora y porque siempre tuve la idea de que la tecnología sería la novedad del futuro.</p>
+
+        <h2 className="section-subtitle">Desarrollo técnico</h2>
+        <p>A lo largo de mi formación, he consolidado una base técnica sólida en desarrollo web y de software. Cuento con experiencia trabajando en el stack MERN (React, Node.js), así como en el entorno .NET con C#. Complemento esto con JavaScript, HTML, CSS, Bootstrap y Tailwind para crear interfaces funcionales y modernas.</p>
+
+        <h2 className="section-subtitle">Más detalles</h2>
+        <div className="bio-details-left">
+          <p><strong>Edad:</strong> 18 años</p>
+          <p><strong>Formación:</strong> Estudiante de Perito Técnico en Kinal</p>
+          <p><strong>Experiencia:</strong> Enfocado en el desarrollo de aplicaciones web y gestión de proyectos.</p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default SobreMi;
