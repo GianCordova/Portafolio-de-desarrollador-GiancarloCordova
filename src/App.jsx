a7441b321d4002627cd5b748c0f0e3f6f12499b0
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-07-22 20:15:41 -0600

    Correcion de configuracion

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Inicio from './pages/Inicio';
 import Sidebar from './components/Sidebar';
 import SobreMi from './pages/SobreMi';
 import Portafolio from './pages/Portafolio';
-import Galeria from './pages/Galeria';  // NUEVO
+import Galeria from './pages/Galeria';
 import Contacto from './pages/Contacto';
 import Educacion from './pages/Educacion';
 import Habilidades from './pages/Habilidades';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portafolio-de-desarrollador-GiancarloCordova/">
       <div className="app-container">
         <Sidebar />
         <main className="content">
@@ -18,7 +18,7 @@ function App() {
             <Route path="/" element={<Inicio />} />
             <Route path="/about" element={<SobreMi />} />
             <Route path="/portafolio" element={<Portafolio />} />
-            <Route path="/galeria" element={<Galeria />} />  
+            <Route path="/galeria" element={<Galeria />} />
             <Route path="/educacion" element={<Educacion />} />
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/habilidades" element={<Habilidades/>}/>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portafolio-de-desarrollador-GiancarloCordova/',
   optimizeDeps: {
     exclude: ['react-icons']
   }
